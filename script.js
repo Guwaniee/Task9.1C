20eff5273470c682d59f674e015aeb1fbe59e7cb
Isuru <isuru@MB-AIR.local>
2023-10-10 19:42:05 +1100

    Enhanced login page and added image

--- script.js
+++ script.js
@@ -1,53 +1,20 @@
-// Get modal elements
+// JavaScript for the login popup
+const loginLink = document.getElementById('loginLink');
 const loginModal = document.getElementById('loginModal');
-const notificationModal = document.getElementById('notificationModal');
+const closeBtn = document.querySelector('.close');
 
-// Get button elements
-const loginBtn = document.getElementById('loginBtn');
-const closeBtn = document.getElementById('closeBtn');
-const okBtn = document.getElementById('okBtn');
-const signOutBtn = document.getElementById('signOutBtn');
-
-// Display the login modal when Login is clicked
-loginBtn.addEventListener('click', () => {
-    loginModal.style.display = 'block';
+loginLink.addEventListener('click', () => {
+  loginModal.style.display = 'block';
 });
 
-// Close the login modal when the close button is clicked
 closeBtn.addEventListener('click', () => {
+  loginModal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === loginModal) {
     loginModal.style.display = 'none';
+  }
 });
 
-// Submit the login form
-const loginForm = document.getElementById('loginForm');
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // Perform login logic here (e.g., validate email and password)
-
-    // Show the notification modal on successful login
-    notificationModal.style.display = 'block';
-    loginModal.style.display = 'none';
-});
-
-// Close the notification modal when OK is clicked
-okBtn.addEventListener('click', () => {
-    notificationModal.style.display = 'none';
-
-    // Show the Sign Out button after successful login
-    signOutBtn.style.display = 'block';
-});
-
-// Show the Sign Out button when OK is clicked (if user is logged in)
-if (localStorage.getItem('loggedIn') === 'true') {
-    signOutBtn.style.display = 'block';
-}
-
-// Handle Sign Out button click
-signOutBtn.addEventListener('click', () => {
-    // Perform sign-out logic here (e.g., clear user session)
-
-    // Hide the Sign Out button after sign-out
-    signOutBtn.style.display = 'none';
-    localStorage.setItem('loggedIn', 'false'); // Update user's login status
-});
+// You can add more JavaScript functionality as needed.
