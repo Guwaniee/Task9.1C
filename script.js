02bd7dad45a0861e6c697f9dde5ae8451ac82bcf
Isuru <isuru@MB-AIR.local>
2023-10-10 20:11:08 +1100

    Enhanced login page and added image

--- script.js
+++ script.js
@@ -17,32 +17,10 @@ function handleLogin() {
       // Hide the login form
       document.getElementById('login-form').style.display = 'none';
 
-      // Show the "Sign Out" button in the navigation bar
+      // Show the "Sign Out" button inside the login form
       document.getElementById('sign-out-button').style.display = 'inline';
   } else {
       alert('Login failed. Please check your credentials.');
-  }
-}
-
-// Function to handle the sign-up process
-function handleSignUp() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  // Check if the entered email already exists in the user array
-  const userExists = users.some(user => user.email === email);
-
-  if (!userExists) {
-      // Add the new user to the users array (for simplicity, we're not doing server-side validation)
-      users.push({ email, password });
-
-      alert('Sign-up successful! You can now log in.');
-
-      // Clear the email and password fields
-      document.getElementById('email').value = '';
-      document.getElementById('password').value = '';
-  } else {
-      alert('Sign-up failed. This email is already registered.');
   }
 }
 
@@ -55,8 +33,4 @@ document.getElementById('sign-out-button').addEventListener('click', function() 
   // Add sign-out logic here
   alert('You have been signed out.');
   // You can also add code to hide the "Sign Out" button after signing out.
-});
-
-document.getElementById('sign-up-button').addEventListener('click', function() {
-  handleSignUp();
 });
