9e8eab5a98d08c4925cc7e4635ba2a7ac90966bd
Isuru <isuru@MB-AIR.local>
2023-10-10 20:18:24 +1100

    Enhanced login page and added image

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
+// Create an array to store user credentials (email and password)
+const users = [
+  { email: 'guwanisd@gmail.com', password: 'Sdgpc201@' }
+];
+
 // Function to handle the login process
 function handleLogin() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  // Check if the entered email and password match any user (add your logic here)
-  if (email === 'guwanisd@gmail.com' && password === 'Sdgpc201@') {
-      alert('Login successful!');
-      
+  // Check if the entered email and password match any user
+  const user = users.find(user => user.email === email && user.password === password);
+
+  if (user) {
       // Hide the login form
       document.getElementById('login-form').style.display = 'none';
 
       // Show the "Sign Out" button
       document.getElementById('sign-out-button').style.display = 'inline';
+
+      // Show a notification with a "Sign Out" button
+      const confirmation = confirm('Login successful!\nClick OK to sign out.');
+      if (confirmation) {
+          // Add sign-out logic here
+          alert('You have been signed out.');
+          // You can also add code to hide the "Sign Out" button after signing out.
+      }
   } else {
       alert('Login failed. Please check your credentials.');
   }
 }
 
 document.getElementById('login-form').addEventListener('submit', function(e) {
-  e.preventDefault(); // Prevent the default form submission
-  handleLogin();
-});
-
-document.getElementById('sign-out-button').addEventListener('click', function() {
-  // Add sign-out logic here
-  alert('You have been signed out.');
-
-  // Show the login form again
-  document.getElementById('login-form').style.display = 'block';
-
-  // Hide the "Sign Out" button
-  document.getElementById('sign-out-button').style.display = 'none';
-});
+  e.preventDefault(); // Prevent the
